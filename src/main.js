a0e4a070958d51bbc288d6f220bb9b92afe3ccfb
Simone Rosso <simone.rosso@itiszuccante.edu.it>
2022-10-16 21:04:02 +0200

    add pwa

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './registerServiceWorker'
 
 createApp(App).mount('#app')
